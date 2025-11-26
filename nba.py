d42b82c589e43cf72276b3ab6debf09c63542636
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-26 09:44:29 +0800

    Update nba.py

--- nba.py
+++ nba.py
@@ -5,7 +5,7 @@ from datetime import datetime
 
 playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/iptv/refs/heads/main/NBA1.m3u",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/NBA%20HOMECOURT.m3u,
+    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/NBA%20HOMECOURT.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbaglobe.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Roxiestreams.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbaleaguepass.m3u",
