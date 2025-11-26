2768c7f0ccce8c4cb6692d7889eff793217d9a00
Bainchiclo <baniknikinchiclo@gmail.com>
2025-11-26 09:52:35 +0800

    Update nba.py

--- nba.py
+++ nba.py
@@ -12,7 +12,7 @@ playlist_urls = [
 ] 
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
-OUTPUT_FILE = "https://github.com/Bainchiclo/nickiptv/blob/main/nbalaptop.m3u8"
+OUTPUT_FILE = "nbalaptop.m3u8"
 
 def fetch_playlist(url, retries=3, timeout=30):
     headers = {"User-Agent": "Mozilla/5.0"}
