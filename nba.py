3d8c227d5906873bd521665a836fb93c92fedec2
Bainchiclo <baniknikinchiclo@gmail.com>
2025-12-21 21:10:56 +0800

    Update nba.py

--- nba.py
+++ nba.py
@@ -9,7 +9,7 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Pixelsports.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Roxiestreams.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/Nba%20Live%20-%20StreamBTW.m3u",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbaleaguepass.m3u"
+    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbastreamgate.m3u"
 ] 
 
 EPG_URL = "https://github.com/Drewski2423/DrewLive/raw/refs/heads/main/DrewLive.xml.gz"
