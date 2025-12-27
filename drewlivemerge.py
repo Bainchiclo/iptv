28fb3345e05ba862036439706dd948588a70fb87
Bainchiclo <baniknikinchiclo@gmail.com>
2025-12-28 07:52:31 +0800

    Update drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -11,6 +11,7 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/fststrm/refs/heads/main/roxstrmz.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/phtest.m3u",
     "https://raw.githubusercontent.com/doms9/iptv/refs/heads/default/M3U8/events.m3u8",
+    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbastreamgate.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/fststrm/refs/heads/main/psports.m3u8"
 ] 
 
