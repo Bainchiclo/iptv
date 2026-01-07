431bc21b49502888cd39368e89ba2d5323b51b0b
Bainchiclo <baniknikinchiclo@gmail.com>
2026-01-07 13:43:52 +0800

    Update webos.py

--- webos.py
+++ webos.py
@@ -6,6 +6,7 @@ from datetime import datetime
 playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbaglobe.m3u",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/cdntv.m3u8",
+    "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/baniknikoffline.m3u8",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/globe.m3u8"
 ] 
 
