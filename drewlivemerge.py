1909eab409bf1db4481d5a57121fee7ce84ab144
Bainchiclo <baniknikinchiclo@gmail.com>
2026-01-08 11:14:21 +0800

    Update drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -7,7 +7,7 @@ playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/PH.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/phfilter.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/NBA%20HOMECOURT.m3u",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbaglobe.m3u",
+    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbalivefilter.m3u8",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/globe.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/fststrm/refs/heads/main/roxstrmz.m3u8",
     "https://raw.githubusercontent.com/doms9/iptv/refs/heads/default/M3U8/events.m3u8",
