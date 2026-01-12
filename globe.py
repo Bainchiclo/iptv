6e3bc0188c1eff440e15f91ec03e47319ed40377
Bainchiclo <baniknikinchiclo@gmail.com>
2026-01-12 09:33:46 +0800

    Update globe.py

--- globe.py
+++ globe.py
@@ -4,7 +4,6 @@ import time
 from datetime import datetime
 
 playlist_urls = [
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbalivefilter.m3u8",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/cdntv.m3u8",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/baniknikoffline.m3u8",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/globe.m3u8"
