e81ec417102c3a14a24ce62433063f7c07368a7d
Bainchiclo <baniknikinchiclo@gmail.com>
2026-01-12 09:32:53 +0800

    Update drewlivemerge.py

--- drewlivemerge.py
+++ drewlivemerge.py
@@ -6,8 +6,6 @@ from datetime import datetime
 playlist_urls = [
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/PH.m3u",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/phfilter.m3u8",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbalivefiltergoogle.m3u8",
-    "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/nbalivefilter.m3u8",
     "https://raw.githubusercontent.com/capdaseletni-sys/kbz/refs/heads/main/globe.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/fststrm/refs/heads/main/roxstrmz.m3u8",
     "https://raw.githubusercontent.com/Bainchiclo/nickiptv/refs/heads/main/liveeventsfilter.m3u8",
